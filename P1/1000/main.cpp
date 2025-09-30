92f246cd96c65b68065edec2e1824580dbef8340
bby12138 <3134930866@qq.com>
2025-09-30 17:51:54 +0800

    Add files via upload

---
+++ P1/1000/main.cpp
@@ -0,0 +1,16 @@
+/*
+HDU OJ 1000
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    while (cin >> a >> b)
+    {
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
